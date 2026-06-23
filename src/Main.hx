6fd2bb78ea9e249710bfa50e127f05163ccd61fb
LukasSkaane <lukasskaane@gmail.com>
2026-06-23 02:02:09 +0200

    Added imgui to the project with a nice demo that I can work off of :D

--- src/Main.hx
+++ src/Main.hx
@@ -1,17 +1,31 @@
+import hxd.res.Image;
+import h2d.Object;
+import imgui.ImGui;
+import imgui.ImGuiDrawable;
 import h2d.TileGroup;
 import h2d.Scene;
 
 class Main extends hxd.App {
 
     var background:Background; 
+    var gui:Gui;
+    
+
     override function init(){
         hxd.Res.initEmbed();
 
         background = new Background(s2d);
         background.init();
+
+        gui = new Gui(s2d);
     }
 
     override function update(dt:Float){
+        gui.update(s2d.getScene(), dt);
+    }
+    
+    override function onResize() {
+        ImGui.setDisplaySize(this.s2d.width, this.s2d.height);
     }
 
     static function main() {
@@ -19,13 +33,35 @@ class Main extends hxd.App {
     }
 }
 
+class Gui{
+    var drawable:ImGuiDrawable;
+
+    public function new(scene:Scene){
+        this.drawable = new ImGuiDrawable(scene);
+
+        ImGui.setDisplaySize(scene.width, scene.height);
+    }
+
+    public function update(scene:Scene, dt:Float){
+        drawable.update(dt);
+
+        ImGui.newFrame();
+
+        ImGui.showDemoWindow();
+
+        ImGui.render();
+    }
+}
+
 class Background {  
     public var background_controller:h2d.Object;
     var bg_obj_dirt:h2d.Object;
+    var bg_image_dirt:Image;
 
     public function new(scene:Scene){
-        var bg_image_dirt = hxd.Res.background.dirt;
-        
+        bg_image_dirt = hxd.Res.background.dirt;
+        scene.addChild(bg_image_dirt);
+
         var bg_dirt = new h2d.TileGroup();
         
     }
@@ -36,7 +72,7 @@ class Background {
         var size = 32;
         for (x in 0...size){
             for (y in 0...size){
-                bg_dirt.addChildAt()
+                // bg_dirt.addChildAt();
             }
         }
     }
