69417cb2ed66997c7841fabe867461cd7fe066ff
LukasSkaane <lukasskaane@gmail.com>
2026-07-01 15:39:45 +0200

    Moved class world_controler to it's own file to keep main clean.

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,5 @@
 import hxd.Window;
-import h2d.Scene;
+
 
 class Main extends hxd.App {
 	var isInitReady = false;
@@ -24,7 +24,7 @@ class Main extends hxd.App {
 
 	override function update(dt:Float) {
 		World_Controller.update(s2d);
-		gui.update(s2d.getScene(), dt);		
+		gui.update(s2d.getScene(), dt);	
 	}
 
 	override function onResize() {
@@ -37,28 +37,4 @@ class Main extends hxd.App {
 	static function main() {
 		new Main();
 	}
-}
-
-final class World_Controller {
-	static var background:Background;
-	static var block_spawner:Block_Spawner;
-
-	static var diggable_width:UInt = 5;
-
-	static var dirt_width:UInt = 16;
-	static var dirt_scale:UInt = 2;
-	
-	static var block_width:UInt = 32;
-	static var block_scale:UInt = 1;
-
-	static public function init(scene:Scene, size:UInt = 32) {
-		background = new Background(scene, dirt_width, dirt_scale, diggable_width);
-		block_spawner = new Block_Spawner(scene, block_width, block_scale, diggable_width);
-	}
-
-	static public function update(scene:Scene){
-		block_spawner.update(scene);
-	}
-
-	static public function onResize() {}
 }
