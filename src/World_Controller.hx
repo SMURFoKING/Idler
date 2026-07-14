34b9d26ecdeda315980c028faae674fe3f475a58
SMURFoKING <lukasskaane@gmail.com>
2026-07-15 01:14:41 +0200

    Added global, state variables for world and blocks. Now inits and creates the blocks too, and stores what it needs in the state. Will likely bring out the different inits to another function, then create everything in a separate step, but that will come later.

--- src/World_Controller.hx
+++ src/World_Controller.hx
@@ -1,12 +1,17 @@
+import Block_Spawner.createBlocks;
+import Block_Spawner.loadOreImages;
+import Block_Spawner.initBlockState;
+import GameState.WorldState;
+import GameState.BlockState;
 import Background;
 import GameState.BackgroundState;
 import h2d.Scene;
 
-var bg_state:BackgroundState;
+var worldState:WorldState;
+var bgState:BackgroundState;
+var blockState:BlockState;
 
 final class World_Controller {
-	static var block_spawner:Block_Spawner;
-
 	static var diggable_width:UInt = 5;
 
 	static var dirt_width:UInt = 16;
@@ -19,21 +24,24 @@ final class World_Controller {
     static var block_start_height:UInt = bg_start_height + 2;
 
 	static public function init(scene:Scene, size:UInt = 32) {
-		bg_state = initBGState(dirt_width, dirt_scale, bg_start_height);
-		bg_state.sides_tilegroup = setSidesTileGroupSettingsForBG(bg_state.sides_tilegroup);
-		bg_state.middle_tilegroup = setMiddleTileGroupSettingsForBG(bg_state.middle_tilegroup);
+		worldState = {
+			diggable_width: diggable_width
+		}		
+
+		bgState = initBGState(dirt_width, dirt_scale, bg_start_height);
+		bgState.sides_tilegroup = setSidesTileGroupSettingsForBG(bgState.sides_tilegroup);
+		bgState.middle_tilegroup = setMiddleTileGroupSettingsForBG(bgState.middle_tilegroup);
 
 		createBG(scene.width, diggable_width);
-		scene.add(bg_state.sides_tilegroup);
-		scene.add(bg_state.middle_tilegroup);
+		scene.add(bgState.sides_tilegroup);
+		scene.add(bgState.middle_tilegroup);
 
-		block_spawner = new Block_Spawner(scene, block_width, block_scale, diggable_width, block_start_height);
-
+		blockState = initBlockState(block_width, block_scale, bg_start_height + 2, scene.width / 2, diggable_width);
+		blockState.blockTiles = loadOreImages();
+		blockState.stoneTile = blockState.blockTiles.getByIndex(0);
+		blockState.activeBlocks = createBlocks(scene, diggable_width);
+		
         Gui_Contraptions.calculateContraptionBoxPositions(scene.width, size, diggable_width, block_start_height);
-	}
-
-	static public function update(scene:Scene){
-		block_spawner.update(scene);
 	}
 
 	static public function onResize() {}
