45f37fcc382db045ac41aa48b31958578b9c1e74
SMURFoKING <lukasskaane@gmail.com>
2026-07-15 01:11:16 +0200

    Renamed state variable for background

--- src/Background.hx
+++ src/Background.hx
@@ -1,4 +1,4 @@
-import World_Controller.bg_state;
+import World_Controller.bgState;
 import GameState.BackgroundState;
 import h2d.TileGroup;
 import h3d.Vector4;
@@ -23,12 +23,12 @@ function initBGState(block_width:UInt, block_scale:UInt, start_height:UInt):Back
 }
 
 function setSidesTileGroupSettingsForBG(tileGroup:TileGroup):TileGroup {
-	tileGroup.setScale(bg_state.scale);
+	tileGroup.setScale(bgState.scale);
 	return tileGroup;
 }
 
 function setMiddleTileGroupSettingsForBG(tileGroup:TileGroup):TileGroup {
-	tileGroup.setScale(bg_state.scale);
+	tileGroup.setScale(bgState.scale);
 	tileGroup.color = new Vector4(0.6, 0.6, 0.65, 1);
 	return tileGroup;
 }
@@ -36,21 +36,21 @@ function setMiddleTileGroupSettingsForBG(tileGroup:TileGroup):TileGroup {
 // still wrong, has hidden side effects: changes state from inside the function of both bg tilegroups.
 // requires splitting function into multiple to fix. Will do additional changes to allow more flexibility so im holding off.
 function createBG(scene_width:UInt, diggable_width:UInt) {
-	var adjusted_scene_middle = scene_width / 2 / bg_state.size;
-	var adjusted_end = Std.int(scene_width / bg_state.size);
+	var adjusted_scene_middle = scene_width / 2 / bgState.size;
+	var adjusted_end = Std.int(scene_width / bgState.size);
 
 	for (x in 0...adjusted_end) {
-		for (y in bg_state.start_height...bg_state.size) {
+		for (y in bgState.start_height...bgState.size) {
 			if (x < adjusted_scene_middle - diggable_width || x >= adjusted_scene_middle + diggable_width) {
-				if (y == bg_state.start_height)
-					bg_state.sides_tilegroup.add(x * bg_state.grass_tile.width, y * bg_state.grass_tile.height, bg_state.grass_tile);
+				if (y == bgState.start_height)
+					bgState.sides_tilegroup.add(x * bgState.grass_tile.width, y * bgState.grass_tile.height, bgState.grass_tile);
 				else
-					bg_state.sides_tilegroup.add(x * bg_state.dirt_tile.width, y * bg_state.dirt_tile.height, bg_state.dirt_tile);
+					bgState.sides_tilegroup.add(x * bgState.dirt_tile.width, y * bgState.dirt_tile.height, bgState.dirt_tile);
 			} else {
-				if (y == bg_state.start_height)
-					bg_state.middle_tilegroup.add(x * bg_state.grass_tile.width, y * bg_state.grass_tile.height, bg_state.grass_tile);
+				if (y == bgState.start_height)
+					bgState.middle_tilegroup.add(x * bgState.grass_tile.width, y * bgState.grass_tile.height, bgState.grass_tile);
 				else
-					bg_state.middle_tilegroup.add(x * bg_state.dirt_tile.width, y * bg_state.grass_tile.height, bg_state.dirt_tile);
+					bgState.middle_tilegroup.add(x * bgState.dirt_tile.width, y * bgState.grass_tile.height, bgState.dirt_tile);
 			}
 		}
 	}
